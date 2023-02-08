01f1a5570b971945714dbbca0df4c53fedf5f1ed
cooderyg <ygk9604@naver.com>
2023-02-08 19:15:18 +0900

    github링크 재수정

--- src/components/TheFooter.js
+++ src/components/TheFooter.js
@@ -11,7 +11,7 @@ export default class TheFooter extends Component {
         // const { github, repository } = aboutStore.state
         this.el.innerHTML = /*html*/ `
             <div>
-                <a href="http://github.com/cooderyg/vanillajs-movie-app">
+                <a href="https://github.com/cooderyg/movie-app">
                     GitHub Repository
                 </a>
             </div>
